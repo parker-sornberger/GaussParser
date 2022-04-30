75af833ce8f5d49ca14043831f34014850a19660
parker-sornberger <60203472+parker-sornberger@users.noreply.github.com>
2022-04-30 10:31:40 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='GaussParser',
+    version='1.0.0',
+    packages=['gaussparser',],
+
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    
+)
